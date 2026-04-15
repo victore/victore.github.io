3115efffb3e00834881188533257511398135eb1
Victor Guadarrama <victor.guadarrama@infosel.com>
2026-04-15 15:07:27 -0600

    add New version

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Victor Guadarrama';
+export const SITE_DESCRIPTION = 'Software Engineer from 🇲🇽 focused on building AI-augmented products that solve real problems at scale.';
